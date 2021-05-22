a1ed6de2fa765ffadb3c2bbf5a86981bcd83ce78
Geomario <marm1984@gmail.com>
2021-05-22 18:28:04 +0200

    Hanlder Function in JSX

--- src/App.js
+++ src/App.js
@@ -29,6 +29,28 @@ const list = [
   },
 ];
 
+// Root Component (Function Component)
+function App() {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+  return(
+    <div>
+      <h1>
+        My Hacker Stories
+      </h1>
+      <h1>
+        Hello {getTitle("Java Function")}
+      </h1>
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text" onChange={handleChange}/>
+      <hr />
+      <List/>
+    </div>
+  );
+}
+
+// Child Component (Function Component)
 function List() {
   return(
     <div>
@@ -45,26 +67,7 @@ function List() {
             <span> {item.age} </span>
           </div>);
         })}
-    </div>
-      );
-}
-
-function App() {
-
-  return(
-    <div>
-      <h1>
-        My Hacker Stories
-      </h1>
-      <h1>
-        Hello {getTitle("Java Function")}
-      </h1>
-      <label htmlFor="search"> Search: </label>
-      <input id="search" type="text"/>
-      <hr />
-      <List/>
-    </div>
-  );
-}
-
+      </div>
+    );
+  }
 export default App;
